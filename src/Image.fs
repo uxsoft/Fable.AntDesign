355b2a69b828d522c81240e3c43676c307d68d88
Jan Dryk <me@uxsoft.cz>
2020-08-24 12:17:08 +0200

    add image component

---
+++ src/Image.fs
@@ -0,0 +1,14 @@
+namespace Fable.AntD
+
+open Browser.Types
+open Fable.React
+
+type AntImage() =
+    inherit AntElement<AntImage>(ofImport "Image" "antd")
+    member x.alt (v: string) = x.attribute "alt" v 
+    member x.fallback (v: string) = x.attribute "fallback" v 
+    member x.height (v: int) = x.attribute "height" v 
+    member x.placeholder (v: ReactElement) = x.attribute "placeholder" v 
+    member x.preview (v: bool) = x.attribute "preview" v 
+    member x.src (v: string) = x.attribute "src" v 
+    member x.width (v: int) = x.attribute "width" v 
