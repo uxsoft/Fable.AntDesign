73fe262c4b57466231b1b08edda06eee18809342
Jan Dryk <me@uxsoft.cz>
2020-09-19 20:53:38 +0200

    add AntDropdown.arrow

--- src/Dropdown.fs
+++ src/Dropdown.fs
@@ -13,26 +13,27 @@ type DropdownTrigger = Click | Hover | ContextMenu
 
 type AntDropdown() =
     inherit AntElement<AntDropdown>(ofImport "Dropdown" "antd")
+    member x.arrow (v: bool) = x.attribute "arrow" v 
     member x.disabled (v: bool) = x.attribute "disabled" v 
     member x.getPopupContainer (v: (unit -> HTMLElement)) = x.attribute "getPopupContainer" v 
     member x.overlay (v: ReactElement) = x.attribute "overlay" v 
     member x.overlayClassName (v: string) = x.attribute "overlayClassName" v
     member x.overlayStyle (css: Props.CSSProp list) = x.attribute "overlayStyle" (keyValueList CaseRules.LowerFirst css)
+    member x.onVisibleChange (v: (bool -> unit)) = x.attribute "onVisibleChange" v 
     member x.placement (v: DropdownPlacement) = x.attribute "placement" v 
     member x.trigger (v: DropdownTrigger array) = x.attribute "trigger" v 
     member x.visible (v: bool) = x.attribute "visible" v 
-    member x.onVisibleChange (v: (bool -> unit)) = x.attribute "onVisibleChange" v 
     
 type AntDropdownButton() =
     inherit AntElement<AntDropdownButton>(ofImport "Dropdown.Button" "antd")
+    member x.buttonsRender (v: ReactElement array array -> ReactElement) = x.attribute "buttonsRender" v
     member x.disabled (v: bool) = x.attribute "disabled" v
-    member x.icon (v: bool) = x.attribute "icon" v
+    member x.icon (v: ReactElement) = x.attribute "icon" v
+    member x.onClick (v: Event -> unit) = x.attribute "onClick" v 
+    member x.onVisibleChange (v: (bool -> unit)) = x.attribute "onVisibleChange" v 
     member x.overlay (v: ReactElement) = x.attribute "disabled" v 
     member x.placement (v: DropdownPlacement) = x.attribute "disabled" v 
     member x.size (v: Size) = x.attribute "size" v
     member x.trigger (v: DropdownTrigger array) = x.attribute "trigger" v
     member x.buttonType (v: ButtonType) = x.attribute "type" v
     member x.visible (v: bool) = x.attribute "visible" v
-    member x.onClick (v: Event -> unit) = x.attribute "onClick" v 
-    member x.onVisibleChange (v: (bool -> unit)) = x.attribute "onVisibleChange" v 
-    member x.buttonsRender (v: ReactElement array array -> ReactElement) = x.attribute "buttonsRender" v
