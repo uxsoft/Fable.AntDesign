04e32f8427d7e82d329b0addc77c64415c55bd5d
Jan Dryk <me@uxsoft.cz>
2020-09-20 09:28:33 +0200

    more flexible simplification of bool attributes in dropdown

--- src/Dropdown.fs
+++ src/Dropdown.fs
@@ -13,8 +13,8 @@ type DropdownTrigger = Click | Hover | ContextMenu
 
 type AntDropdown() =
     inherit AntElement<AntDropdown>(ofImport "Dropdown" "antd")
-    member x.arrow () = x.attribute "arrow" true 
-    member x.disabled () = x.attribute "disabled" true 
+    member x.arrow (?v: bool) = x.attribute "arrow" (Option.defaultValue true v) 
+    member x.disabled (?v: bool) = x.attribute "disabled" (Option.defaultValue true v) 
     member x.getPopupContainer (v: (unit -> HTMLElement)) = x.attribute "getPopupContainer" v 
     member x.overlay (v: ReactElement) = x.attribute "overlay" v 
     member x.overlayClassName (v: string) = x.attribute "overlayClassName" v
@@ -22,12 +22,12 @@ type AntDropdown() =
     member x.onVisibleChange (v: (bool -> unit)) = x.attribute "onVisibleChange" v 
     member x.placement (v: DropdownPlacement) = x.attribute "placement" v 
     member x.trigger (v: DropdownTrigger array) = x.attribute "trigger" v 
-    member x.visible (v: bool) = x.attribute "visible" v 
+    member x.visible (?v: bool) = x.attribute "visible" (Option.defaultValue true v) 
     
 type AntDropdownButton() =
     inherit AntElement<AntDropdownButton>(ofImport "Dropdown.Button" "antd")
     member x.buttonsRender (v: ReactElement array array -> ReactElement) = x.attribute "buttonsRender" v
-    member x.disabled () = x.attribute "disabled" true
+    member x.disabled (?v: bool) = x.attribute "disabled" (Option.defaultValue true v)
     member x.icon (v: ReactElement) = x.attribute "icon" v
     member x.onClick (v: Event -> unit) = x.attribute "onClick" v 
     member x.onVisibleChange (v: (bool -> unit)) = x.attribute "onVisibleChange" v 
@@ -36,4 +36,4 @@ type AntDropdownButton() =
     member x.size (v: Size) = x.attribute "size" v
     member x.trigger (v: DropdownTrigger array) = x.attribute "trigger" v
     member x.buttonType (v: ButtonType) = x.attribute "type" v
-    member x.visible () = x.attribute "visible" true
+    member x.visible (?v: bool) = x.attribute "visible" (Option.defaultValue true v)
