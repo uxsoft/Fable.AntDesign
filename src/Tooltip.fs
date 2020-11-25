f32d535f5c14623102a505e7069fd3e8ee0ce235
Jan Dryk <me@uxsoft.cz>
2020-11-25 22:46:37 +0100

    add map of icons for dynamic creation

--- src/Tooltip.fs
+++ src/Tooltip.fs
@@ -15,10 +15,12 @@ type TooltipTrigger = Hover | Focus | Click | ContextMenu
 
 type AntTooltipBase<'T when 'T :> AntElement<'T>>(partialImport) =
     inherit AntElement<'T>(partialImport)
+    member x.align (v: obj) = x.attribute "align" v
     member x.arrowPointAtCenter (?v: bool) = x.attribute "arrowPointAtCenter" (Option.defaultValue true v)
     member x.autoAdjustOverflow (?v: bool) = x.attribute "autoAdjustOverflow" (Option.defaultValue true v)
-    member x.defaultVisible (?v: bool) = x.attribute "defaultVisible" (Option.defaultValue true v)
     member x.color (v: string) = x.attribute "color" v
+    member x.defaultVisible (?v: bool) = x.attribute "defaultVisible" (Option.defaultValue true v)
+    member x.destroyTooltipOnHide (v: bool) = x.attribute "destroyTooltipOnHide" v
     member x.getPopupContainer (v: (unit -> HTMLElement)) = x.attribute "getPopupContainer" v 
     member x.mouseEnterDelay (v: float) = x.attribute "mouseEnterDelay" v 
     member x.mouseLeaveDelay (v: float) = x.attribute "mouseLeaveDelay" v 
@@ -28,8 +30,6 @@ type AntTooltipBase<'T when 'T :> AntElement<'T>>(partialImport) =
     member x.trigger (v: TooltipTrigger) = x.attribute "trigger" v 
     member x.visible (?v: bool) = x.attribute "visible" (Option.defaultValue true v) 
     member x.onVisibleChange (v: (bool -> unit)) = x.attribute "onVisibleChange" v 
-    member x.align (v: obj) = x.attribute "align" v
-    member x.destroyTooltipOnHide (v: bool) = x.attribute "destroyTooltipOnHide" v
 
 type AntPopover() =
     inherit AntTooltipBase<AntPopover>(ofImport "Popover" "antd")
@@ -38,16 +38,16 @@ type AntPopover() =
 
 type AntPopconfirm() =
     inherit AntTooltipBase<AntPopconfirm>(ofImport "Popconfirm" "antd")
+    member x.cancelButtonProps (v: AntButton) = x.attribute "cancelButtonProps" v.JSON
     member x.cancelText (v: string) = x.attribute "cancelText" v
+    member x.disabled (v: bool) = x.attribute "disabled" v
+    member x.icon (v: ReactElement) = x.attribute "icon" v
+    member x.okButtonProps (v: AntButton) = x.attribute "okButtonProps" v.JSON
     member x.okText (v: string) = x.attribute "okText" v
     member x.okType (v: ButtonType) = x.attribute "okType" v
-    member x.okButtonProps (v: AntButton) = x.attribute "okButtonProps" v.JSON
-    member x.cancelButtonProps (v: AntButton) = x.attribute "cancelButtonProps" v.JSON
     member x.title (v: ReactElement) = x.attribute "title" v
     member x.onCancel (v: Func<unit, unit>) = x.attribute "onCancel" v
     member x.onConfirm (v: Func<unit, unit>) = x.attribute "onConfirm" v
-    member x.icon (v: ReactElement) = x.attribute "icon" v
-    member x.disabled (v: bool) = x.attribute "disabled" v
 
 type AntTooltip() =
     inherit AntTooltipBase<AntTooltip>(ofImport "Tooltip" "antd")
