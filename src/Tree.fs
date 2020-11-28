206b6c7078ea034884937e2dbf47d4f394c644f4
Jan Dryk <me@uxsoft.cz>
2020-11-28 10:35:43 +0100

    define more tree events

--- src/Tree.fs
+++ src/Tree.fs
@@ -10,30 +10,55 @@ type DirectoryTreeExpandAction =
     | Click
     | DoubleClick
 
-type TreeData = {
-    key: string
-    title: string
-    icon: ReactElement
-    children: TreeData array
-    disabled: bool
-    selectable: bool
-}
+type TreeData =
+    {
+        key: string
+        title: string
+        icon: ReactElement
+        children: TreeData array
+        disabled: bool
+        selectable: bool
+    }
 
-type TreeMouseEvent = {
-    event: Event
-    node: TreeData
-}
+type TreeMouseEvent<'T> =
+    {
+        event: Event
+        node: 'T
+    }
 
-type TreeDropEvent = {
-    event: Event
-    node: TreeData
-    dragNode: TreeData
-    dragNodesKeys: string array
-    dropPosition: int
-    dropToGap: bool
-}
+type TreeDropEvent<'T> =
+    {
+        event: Event
+        node: 'T
+        dragNode: 'T
+        dragNodesKeys: string array
+        dropPosition: int
+        dropToGap: bool
+    }
 
-type AntTreeBase<'T when 'T :> AntElement<'T>>(partialImport) =
+type TreeSelectedEvent<'T> =
+    {
+        selected: bool
+        selectedNodes: 'T array
+        node: 'T
+        event: Event
+    }
+    
+type TreeExpandEvent<'T> =
+    {
+        expanded: bool
+        node: 'T
+    }
+    
+type TreeCheckEvent<'T> =
+    {
+        ``checked``: bool
+        checkedNodes: 'T array
+        event: Event
+        halfCheckedKeys: string array
+    }
+
+type AntTreeBase<'TEntity, 'T when 'T :> AntElement<'T>>(partialImport) =
     inherit AntElement<'T>(partialImport)
     member x.autoExpandParent (v: bool) = x.attribute "autoExpandParent" v
     member x.blockNode (?v: bool) = x.attribute "blockNode" (Option.defaultValue true v)
@@ -48,10 +73,10 @@ type AntTreeBase<'T when 'T :> AntElement<'T>>(partialImport) =
     member x.disabled (?v: bool) = x.attribute "disabled" (Option.defaultValue true v)
     member x.draggable (?v: bool) = x.attribute "draggable" (Option.defaultValue true v)
     member x.expandedKeys (v: string array) = x.attribute "expandedKeys" v
-    member x.filterTreeNode (v: TreeData -> bool) = x.attribute "filterTreeNode" v
+    member x.filterTreeNode (v: 'TEntity -> bool) = x.attribute "filterTreeNode" v
     member x.height (v: float) = x.attribute "height" v
     member x.icon (v: ReactElement) = x.attribute "icon" v
-    member x.loadData (v: obj -> unit) = x.attribute "loadData" v
+    member x.loadData (v: 'T -> unit) = x.attribute "loadData" v
     member x.loadedKeys (v: string array) = x.attribute "loadedKeys" v
     member x.multiple (?v: bool) = x.attribute "multiple" (Option.defaultValue true v)
     member x.selectable (?v: bool) = x.attribute "selectable" (Option.defaultValue true v)
@@ -59,26 +84,26 @@ type AntTreeBase<'T when 'T :> AntElement<'T>>(partialImport) =
     member x.showIcon (?v: bool) = x.attribute "showIcon" (Option.defaultValue true v)
     member x.showLine (?v: bool) = x.attribute "showLine" (Option.defaultValue true v)
     member x.switcherIcon (v: ReactElement) = x.attribute "switcherIcon" v
-    member x.titleRender (v: Func<TreeData, ReactElement>) = x.attribute "titleRender" v
-    member x.treeData (v: TreeData array) = x.attribute "treeData" v
+    member x.titleRender (v: Func<'TEntity, ReactElement>) = x.attribute "titleRender" v
+    member x.treeData (v: 'TEntity array) = x.attribute "treeData" v
     member x.isVirtual (?v: bool) = x.attribute "virtual" (Option.defaultValue true v)
-    member x.onCheck (v: Func<string array, obj, unit>) = x.attribute "onCheck" v
-    member x.onDragEnd (v: TreeMouseEvent -> unit) = x.attribute "onDragEnd" v
-    member x.onDragEnter (v: TreeMouseEvent -> unit) = x.attribute "onDragEnter" v
-    member x.onDragLeave (v: TreeMouseEvent -> unit) = x.attribute "onDragLeave" v
-    member x.onDragOver (v: TreeMouseEvent -> unit) = x.attribute "onDragOver" v
-    member x.onDragStart (v: TreeMouseEvent -> unit) = x.attribute "onDragStart" v
-    member x.onDrop (v: TreeDropEvent -> unit) = x.attribute "onDrop" v
-    member x.onExpand (v: Func<string array, obj, unit>) = x.attribute "onExpand" v
-    member x.onLoad (v: Func<string array, obj, unit>) = x.attribute "onLoad" v
-    member x.onRightClick (v: TreeMouseEvent -> unit) = x.attribute "onRightClick" v
-    member x.onSelect (v: Func<string array, obj, unit>) = x.attribute "onSelect" v
+    member x.onCheck (v: Func<string array, TreeCheckEvent<'T>, unit>) = x.attribute "onCheck" v
+    member x.onDragEnd (v: TreeMouseEvent<'TEntity> -> unit) = x.attribute "onDragEnd" v
+    member x.onDragEnter (v: TreeMouseEvent<'TEntity> -> unit) = x.attribute "onDragEnter" v
+    member x.onDragLeave (v: TreeMouseEvent<'TEntity> -> unit) = x.attribute "onDragLeave" v
+    member x.onDragOver (v: TreeMouseEvent<'TEntity> -> unit) = x.attribute "onDragOver" v
+    member x.onDragStart (v: TreeMouseEvent<'TEntity> -> unit) = x.attribute "onDragStart" v
+    member x.onDrop (v: TreeDropEvent<'TEntity> -> unit) = x.attribute "onDrop" v
+    member x.onExpand (v: Func<string array, TreeExpandEvent<'T>, unit>) = x.attribute "onExpand" v
+    member x.onLoad (v: Func<string array, TreeMouseEvent<'T>, unit>) = x.attribute "onLoad" v
+    member x.onRightClick (v: TreeMouseEvent<'TEntity> -> unit) = x.attribute "onRightClick" v
+    member x.onSelect (v: Func<string array, TreeSelectedEvent<'T>, unit>) = x.attribute "onSelect" v
 
-type AntTree() =
-    inherit AntTreeBase<AntTree>(ofImport "Tree" "antd")
+type AntTree<'T>() =
+    inherit AntTreeBase<'T, AntTree<'T>>(ofImport "Tree" "antd")
 
-type AntDirectoryTree() =
-    inherit AntTreeBase<AntDirectoryTree>(ofImport "Tree.DirectoryTree" "antd")
+type AntDirectoryTree<'T>() =
+    inherit AntTreeBase<'T, AntDirectoryTree<'T>>(ofImport "Tree.DirectoryTree" "antd")
     member x.expandAction (v: DirectoryTreeExpandAction) = x.attribute "expandAction" v
 
 type AntTreeNode() =
