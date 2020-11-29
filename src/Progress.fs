5bb547ca4f5465d2a0e1bab1a8b5bd4a1dbb6807
Jan Dryk <me@uxsoft.cz>
2020-11-29 18:12:10 +0100

    update progress to 4.8.5

--- src/Progress.fs
+++ src/Progress.fs
@@ -18,14 +18,14 @@ type ProgressPosition = Top | Left | Bottom | Right
 
 type AntProgress()  =
     inherit AntElement<AntProgress>(ofImport "Progress" "antd")
-    member x.format (v: (Func<int, {| percent: int; strokeColor: string option |}, string>)) = x.attribute "format" v
+    member x.format (v: (Func<int, int, string>)) = x.attribute "format" v
     member x.percent (v: int) = x.attribute "percent" v
     member x.showInfo (?v: bool) = x.attribute "showInfo" (Option.defaultValue true v)
     member x.status (v: ProgressStatus) = x.attribute "status" v
     member x.strokeColor (v: string) = x.attribute "strokeColor" v
     member x.strokeLinecap (v: ProgressLinecap) = x.attribute "strokeLinecap" v
     member x.strokeWidth (v: int) = x.attribute "strokeWidth" v
-    member x.success (v: {| percent: int; strokeColor: string option |}) = x.attribute "success" {| percent = v |}
+    member x.success (v: {| percent: int; strokeColor: string option |}) = x.attribute "success" v
     member x.trailColor (v: string) = x.attribute "trailColor" v
     member x.width (v: string) = x.attribute "width" v
     member x.gapDegree (v: int) = x.attribute "gapDegree" v
