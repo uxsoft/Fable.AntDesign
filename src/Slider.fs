a4d813f0c41185fc47866f4c2f0eb85dd013d5b7
Jan Dryk <me@uxsoft.cz>
2021-05-08 00:29:22 +0200

    fix slider referencing the wrong component

--- src/Slider.fs
+++ src/Slider.fs
@@ -5,11 +5,13 @@ open Fable.Core
 open Fable.React
 
 type AntSlider() =
-    inherit AntElement<AntSlider>(ofImport "Avatar" "antd")
+    inherit AntElement<AntSlider>(ofImport "Slider" "antd")
     member x.autoFocus (?v: bool) = x.attribute "autoFocus" (Option.defaultValue true v)
-    member x.defaultValue (v: float array) = x.attribute "defaultValue" v
+    member x.defaultValue (v: float) = x.attribute "defaultValue" v
+    member x.defaultValues (v: float array) = x.attribute "defaultValue" v
     member x.disabled (?v: bool) = x.attribute "disabled" (Option.defaultValue true v)
     member x.dots (?v: bool) = x.attribute "dots" (Option.defaultValue true v)
+    member x.getTooltipPopupContainer (v: (unit -> HTMLElement)) = x.attribute "getTooltipPopupContainer" v
     member x.included (?v: bool) = x.attribute "included" (Option.defaultValue true v)
     member x.marks (v: obj) = x.attribute "marks" v
     member x.max (v: float) = x.attribute "max" v
@@ -18,10 +20,9 @@ type AntSlider() =
     member x.reverse (?v: bool) = x.attribute "reverse" (Option.defaultValue true v)
     member x.step (v: int) = x.attribute "step" v
     member x.tipFormatter (v: (float -> ReactElement)) = x.attribute "tipFormatter" v
+    member x.tooltipPlacement (v: TooltipPlacement) = x.attribute "tooltipPlacement" v
+    member x.tooltipVisible (?v: bool) = x.attribute "tooltipVisible" (Option.defaultValue true v)
     member x.value (v: U2<float, float array>) = x.attribute "value" v
     member x.vertical (v: bool) = x.attribute "vertical" v
     member x.onAfterChange (v: (float -> unit)) = x.attribute "onAfterChange" v
     member x.onChange (v: (float -> unit)) = x.attribute "onChange" v
-    member x.tooltipPlacement (v: TooltipPlacement) = x.attribute "tooltipPlacement" v
-    member x.tooltipVisible (?v: bool) = x.attribute "tooltipVisible" (Option.defaultValue true v)
-    member x.getTooltipPopupContainer (v: (unit -> HTMLElement)) = x.attribute "getTooltipPopupContainer" v
