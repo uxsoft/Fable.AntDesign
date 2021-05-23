d9ef729ade94e02393b4530c1c1ae7e9b68ecf03
Jan Dryk <jan.dryk@merck.com>
2021-05-23 14:16:20 +0200

    reorganise to add an examples project and allow for Builder style bindings

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// Note this only includes basic configuration for development mode.
+// For a more comprehensive configuration check:
+// https://github.com/fable-compiler/webpack-config-template
+
+var path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/Fable.AntD.Examples/App.fs.js",
+    output: {
+        path: path.join(__dirname, "./src/Fable.AntD.Examples/public"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: "./src/Fable.AntD.Examples/public",
+        port: 8080,
+    }
+}
