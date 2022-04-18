90695052f79117659df1a12741895eecb2361eb2
Jan Dryk <me@uxsoft.cz>
2022-04-18 16:00:27 +0200

    remove other libraries, they were extracted to their own individual repositories

--- src/Fable.Builders.React/FragmentBuilder.fs
+++
@@ -1,9 +0,0 @@
-module Fable.Builders.React.FragmentBuilder
-
-open Fable.Builders.Common
-
-type FragmentBuilder() =
-    inherit ReactBuilder()
-
-    member _.Run(s: DSLElement) =
-        Fable.React.Helpers.fragment [] s.Children
